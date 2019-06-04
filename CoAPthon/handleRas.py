d8e35e6cf7c362d93ae519cb455076d96ef9acf6
gkrudah <gkrudah@hanyang.ac.kr>
2019-06-04 18:53:32 +0900

    buzzer

--- CoAPthon/handleRas.py
+++ CoAPthon/handleRas.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 # from picamera import PiCamera
 
 pirPin = 12
+buzzerPin = 24
 
 
 class machine(threading.Thread):
@@ -13,18 +14,32 @@ class machine(threading.Thread):
 
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(pirPin, GPIO.IN)
+		GPIO.setup(buzzerPin, GPIO.OUT)
+
+		# buzzer init
+		GPIO.setup(buzzerPin, False)
+		# self.buzzer = GPIO.PWM(buzzerPin, 100)
+		# self.buzzer.start(0)
+		# self.buzzer.ChangeDutyCycle(90)
 
 		# self.lock = lock
 		return
 
-	def checkmachine(self, interval=0.8):
+	def ringBuzzer(self):
+		GPIO.output(buzzerPin, True)
+		sleep(0.5)
+		GPIO.output(buzzerPin, False)
+		return
+
+	def checkmachine(self, interval=1.0):
 
 		if GPIO.input(pirPin):
 			self.jsondata.setState('on')
+			self.ringBuzzer()
 		else:
 			self.jsondata.setState('off')
 
-		threading.Thread(interval, self.checkmachine).start()
+		threading.Timer(interval, self.checkmachine).start()
 		return
 
 	def run(self):
